4636672f424d136394047a5e51c0bcb6af47754e
Aabir Khimani <aabirk123@gmail.com>
2021-07-01 18:24:06 -0400

    website code

---
+++ frontend/src/components/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+import {render} from "react-dom";
+import HomePage from "./HomePage";
+
+export default class App extends Component{
+    constructor(props) {
+        super(props);
+
+    }
+    render(){
+        return(
+            <div className="center">
+                <HomePage />
+            </div>
+        )
+    }
+}
+
+const AppDiv = document.getElementById("app");
+render(<App />, AppDiv)
---
+++ spotify/credentials.py
@@ -0,0 +1,3 @@
+CLIENT_ID = "2c3362e1fa924f8d9a1d5e6d2b6be2f7"
+CLIENT_SECRET = "1dc0596071fe4f05953495ec3e5b02ab"
+REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
